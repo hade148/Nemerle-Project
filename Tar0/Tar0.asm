d0a1b07863b8ebb15f6c71810622a3aef936b6c4
Hasada Ester Elbaz <hadelbaz@g.jct.ac.il>
2026-05-05 13:13:46 +0300

    update

---
+++ Tar0/Tar0.asm
@@ -0,0 +1,30 @@
+InputA
+### BUY bread ###
+13.5
+### BUY cheese ###
+10.6
+$$$ CELL cheese $$$
+85.5
+### BUY milk ###
+24.8
+$$$ CELL chocolate $$$
+71.4
+### BUY milk ###
+22.4
+$$$ CELL chocolate $$$
+11
+InputB
+### BUY chocolate ###
+12.5
+$$$ CELL eggs $$$
+621
+### BUY cheese ###
+10.6
+$$$ CELL sugar $$$
+82.5
+$$$ CELL flour $$$
+25.2
+### BUY milk ###
+11.1
+TOTAL BUY: 105.5
+TOTAL CELL: 896.6
